06bfdf01fca836c39d92dcb9481efda472300c55
Timo Haapio <timohaapio@Timo-iMac.local>
2017-02-06 17:25:35 +0200

    album files

---
+++ src/components/AlbumList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios.get('https://raw.githubusercontent.com/haapio/apis/master/udemy/the-complete-react-native-and-redux-course/albums/albums.json')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.id} album={album} />
+    );
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        {this.renderAlbums()}
+      </ScrollView>
+    );
+  }
+}
+
+export default AlbumList;
